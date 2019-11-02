1b5cc63e32783b3628e2261eb45e982a92380344
Matt 88 <slainwarrior@hotmail.com>
2019-11-02 20:36:37 +0000

    Neater haskell solution

--- 2018/2/main.hs
+++ 2018/2/main.hs
@@ -6,26 +6,14 @@ import Data.List
 -- import Data.Map
 
 
-twosAndThrees :: [String] -> (Bool, Bool)
-twosAndThrees [] = (False, False) 
-twosAndThrees (x:xs) = smush (two, three) (twosAndThrees xs)
-    where 
-        two   = length x == 2
-        three = length x == 3
+hasLetterCount :: Int -> String -> Bool
+hasLetterCount l code = any (lengthOf l) (group $ sort $ code)
 
-smush :: (Bool, Bool) -> (Bool, Bool) -> (Bool, Bool)
-smush (a, b) (c, d) = (a || c, b || d)
-
-parseCode :: String -> (Int, Int)
-parseCode x = both fromEnum (twosAndThrees $ group $ sort $ x)
-
-parseCodes :: [String] -> Int
-parseCodes x = combine (map parseCode x)
-
-combine :: [(Int, Int)] -> Int
-combine x = (sum $ map fst x) * (sum $ map snd x)
+lengthOf :: Int -> String -> Bool
+lengthOf l x = length x == l
 
 main = do
     file_lines <- fmap Text.lines (Text.readFile "input.txt")
     let lines = map Text.unpack file_lines
-    putStrLn $ show $ parseCodes lines
+    let answer_1 = (length $ filter (hasLetterCount 2) lines) * (length $ filter (hasLetterCount 3) lines)
+    putStrLn $ show $ answer_1
