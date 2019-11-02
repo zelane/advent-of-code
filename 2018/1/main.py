5858d9067d08626581da6ee0585d48c5b38cae17
Matt 88 <slainwarrior@hotmail.com>
2019-11-02 20:14:56 +0000

    Python solution

---
+++ 2018/1/main.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+with open('src/input.txt') as f:
+    input = tuple(int(line) for line in f.readlines())
+
+print(sum(input))
+
+input = deque(input)
+previous_values = set()
+current_value = 0
+
+while True:
+    current_value += input[0]
+    
+    if current_value in previous_values:
+        print(current_value)
+        break
+
+    previous_values.add(current_value)
+    input.rotate(-1)
