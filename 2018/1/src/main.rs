bee78e75ed56b25772a001e1dddb108ee19b1f59
Matt 88 <slainwarrior@hotmail.com>
2019-11-02 02:02:21 +0000

    Improve rust impl

--- 2018/1/src/main.rs
+++ 2018/1/src/main.rs
@@ -1,22 +1,41 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::collections::{HashSet, VecDeque};
 
 fn main() -> std::io::Result<()> {
     let file = File::open("src/input.txt")?;
     let reader = BufReader::new(file);
+    let mut values = Vec::new();
+    
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let (sign, value) = line.split_at(1);
+        let mut value: i32 = value.parse().unwrap();
 
+        if sign == "-" {
+           value *= -1;
+        }
+        values.push(value)
+    }
+
+    let sum: i32 = values.iter().sum();
+    println!("{}", sum);
+
+    let mut values = VecDeque::from(values);
+    let mut frequencies = HashSet::new();
     let mut freq = 0;
 
-    for line in reader.lines() {
-        let mut line = line.unwrap();
-        let value = line.split_off(1);
-        let mut int: i32 = value.parse().unwrap();
+    loop {
+        let cur_val = values.pop_front().unwrap();
+        freq += cur_val;
 
-        if line == "-" {
-            int = -1 * int;
+        if frequencies.contains(&freq) {
+            println!("{}", freq);
+            break;
         }
-        freq += int;
+        frequencies.insert(freq);
+        values.push_back(cur_val); 
     }
-    println!("{}", freq);
+
     Ok(())
 }
