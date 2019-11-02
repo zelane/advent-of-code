4e01b88717838e93f9f74f719afc26c6aed9decc
Zelane <matt.maglennon@tails.com>
2019-11-02 23:39:56 +0000

    partial day 2b haskell

--- 2018/2/main.hs
+++ 2018/2/main.hs
@@ -12,8 +12,22 @@ lengthOf l x = length x == l
 matchCodes :: [String] -> Int -> Int
 matchCodes codes l = length $ filter (hasLetterCount l) codes
 
+commonLetters :: String -> String -> [Char] -> [Char]
+commonLetters [] [] x = x
+commonLetters (a:as) (b:bs) x = if a == b then commonLetters as bs (x ++ [a]) else commonLetters as bs x
+
+findMatch :: [String] -> String -> Maybe String
+findMatch [] x = Nothing
+findMatch (l:ls) x = if length common == 25 then Just common else findMatch ls x
+    where
+        common = commonLetters x l []
+
 main = do
     file_lines <- fmap Text.lines (Text.readFile "input.txt")
     let lines = map Text.unpack file_lines
+
     let answer_1 = (matchCodes lines 2) * (matchCodes lines 3)
     putStrLn $ show $ answer_1
+
+    let answer_2 = map (findMatch lines) lines
+    putStrLn $ show $ answer_2
