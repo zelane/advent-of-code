f3831c5f00223e27aedf0fcaaa67efde10104a0b
Zelane <matt.maglennon@tails.com>
2019-11-03 02:24:32 +0000

    Haskell day 3 notes

--- 2018/3/main.hs
+++ 2018/3/main.hs
@@ -6,4 +6,7 @@ main = do
     file_lines <- fmap Text.lines (Text.readFile "input.txt")
     let lines = map Text.unpack file_lines
 
+    -- convert to x, y
+    -- sort, group and count
+
     putStrLn $ show $ 0
