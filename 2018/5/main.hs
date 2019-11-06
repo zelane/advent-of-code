ef6065bb94201a77833dd2809a748736b5e9d71b
Matt 88 <slainwarrior@hotmail.com>
2019-11-06 01:26:17 +0000

    Haskell 5a much much faster with Seq

--- 2018/5/main.hs
+++ 2018/5/main.hs
@@ -1,6 +1,7 @@
-import Data.List
 import Data.Char
-import Debug.Trace
+import Data.Sequence hiding (length)
+import qualified Data.Sequence as S (length)
+
 
 swapCase :: Char -> Char
 swapCase a = if isLower a then toUpper a else toLower a
@@ -8,12 +9,19 @@ swapCase a = if isLower a then toUpper a else toLower a
 react :: Char -> Char -> Bool
 react a b = if a == swapCase b then True else False
 
-chainReact :: [Char] -> [Char] -> [Char]
-chainReact [] a    = a
-chainReact (x:xs) [] = chainReact xs [x]
-chainReact (x:xs) a = if react (last a) x then chainReact xs (init a) else chainReact xs (a ++ [x])
+chainReact :: Seq Char -> Seq Char -> Seq Char
+chainReact Empty  a         = a
+chainReact (x :<| xs) Empty = chainReact xs (singleton x)
+chainReact (x :<| xs) a     = if react (lastS a) x then chainReact xs (initS a) else chainReact xs (a |> x)
+
+lastS :: Seq Char -> Char
+lastS (xs :|> x) = x
+
+initS :: Seq Char -> Seq Char
+initS (xs :|> x) = xs
 
 main = do
     polymer <- readFile "input.txt"
-    putStrLn $ show $ length $ chainReact polymer []
+    let poly = fromList polymer
+    putStrLn $ show $ S.length $ chainReact poly empty
  
