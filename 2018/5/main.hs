5d9b2e764edfd5749c05ac4342b88bca9bfff469
Matt 88 <slainwarrior@hotmail.com>
2019-11-06 22:54:56 +0000

    5b Haskell finished

--- 2018/5/main.hs
+++ 2018/5/main.hs
@@ -1,6 +1,7 @@
 import Data.Char
-import Data.Sequence hiding (length)
-import qualified Data.Sequence as S (length)
+import Data.Sequence hiding         (length)
+import qualified Data.Sequence as S (length, filter)
+import qualified Data.Set      as Set
 
 
 swapCase :: Char -> Char
@@ -10,18 +11,32 @@ react :: Char -> Char -> Bool
 react a b = if a == swapCase b then True else False
 
 chainReact :: Seq Char -> Seq Char -> Seq Char
-chainReact Empty  a         = a
-chainReact (x :<| xs) Empty = chainReact xs (singleton x)
-chainReact (x :<| xs) a     = if react (lastS a) x then chainReact xs (initS a) else chainReact xs (a |> x)
+chainReact Empty result         = result
+chainReact (unit:<|poly) Empty  = chainReact poly (singleton unit)
+chainReact (unit:<|poly) result = if react lastUnit unit
+                                  then chainReact poly rs
+                                  else chainReact poly (result |> unit)
+    where
+        (rs:|>lastUnit) = result
 
-lastS :: Seq Char -> Char
-lastS (xs :|> x) = x
+filterLetter :: Seq Char -> Char -> Seq Char
+filterLetter polymer unit = S.filter f polymer
+    where 
+        f x = (toLower x) /= unit
 
-initS :: Seq Char -> Seq Char
-initS (xs :|> x) = xs
+findShortest :: Seq Char -> [Char] -> Int -> Int
+findShortest polymer [] x     = x
+findShortest polymer (c:cs) x = findShortest polymer cs longest
+    where 
+        fpolymer   = filterLetter polymer c
+        polyLength = S.length $ chainReact fpolymer empty
+        longest    = min polyLength x
 
 main = do
     polymer <- readFile "input.txt"
     let poly = fromList polymer
-    putStrLn $ show $ S.length $ chainReact poly empty
- 
+    let polyLen = S.length $ chainReact poly empty
+    putStrLn $ show $ polyLen
+    
+    let allLetters = Set.toList $ Set.fromList $ map toLower polymer
+    putStrLn $ show $ findShortest poly allLetters polyLen
