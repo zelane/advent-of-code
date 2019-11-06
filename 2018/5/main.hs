93b6f1dc6a77b7146095cc32b19d657922632382
Matt 88 <slainwarrior@hotmail.com>
2019-11-06 00:42:30 +0000

    Python 5ab done, Haskell 5a done

--- 2018/5/main.hs
+++ 2018/5/main.hs
@@ -2,23 +2,18 @@ import Data.List
 import Data.Char
 import Debug.Trace
 
+swapCase :: Char -> Char
+swapCase a = if isLower a then toUpper a else toLower a
+
 react :: Char -> Char -> Bool
-react a b = if (toLower a) /= (toLower b) || (isLower a) == (isLower b) then False else True
+react a b = if a == swapCase b then True else False
 
-slide :: (Char -> Char -> Bool) -> [Char] -> [Char] -> [Char]
-slide _ [] a       = a
-slide _ [x] a      = a ++ [x]
-slide f (x:y:xs) a = if react x y then slide f xs a else slide f (y:xs) (a ++ [x])
-
-repeated :: ([Char] -> [Char] -> [Char]) -> [Char] -> [Char]
-repeated f polymer = if (length polymer) /= traceShow (length new_polymer) (length new_polymer) then repeated f new_polymer else new_polymer
-    where
-        new_polymer = f polymer []
+chainReact :: [Char] -> [Char] -> [Char]
+chainReact [] a    = a
+chainReact (x:xs) [] = chainReact xs [x]
+chainReact (x:xs) a = if react (last a) x then chainReact xs (init a) else chainReact xs (a ++ [x])
 
 main = do
     polymer <- readFile "input.txt"
-    -- let polymer = "dabAcCaCBAcCcaDA"
-    -- putStrLn $ show $ map (take 2) (tails polymer)
-    -- putStrLn $ show $ slide react polymer []
-    putStrLn $ show $ repeated (slide react) polymer
+    putStrLn $ show $ length $ chainReact polymer []
  
