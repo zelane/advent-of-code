b0a6528175e00c0a6508597f44ede724e3d75bf0
Matt 88 <slainwarrior@hotmail.com>
2019-11-09 14:22:21 +0000

    Fix bug in 7a Python

--- 2018/7/main.py
+++ 2018/7/main.py
@@ -1,19 +1,23 @@
 import re
 
 
-with open('input.txt') as f:
+with open('test.txt') as f:
     steps = {
         tuple(re.findall(r" ([A-Z]) ", line)) for line in f.readlines()
     }
 
-answer_1 = ""
-while True:
-    pre_reqs = {step[1] for step in steps}
-    done = sorted(s[0] for s in steps if s[0] not in pre_reqs)[0]
-    answer_1 += done
-    steps = {step for step in steps if step[0] != done}
-    if len(pre_reqs) == 1: break
+def process(steps, workers, time):
+    result = ""
+    while True:
+        pre_reqs = {step[1] for step in steps}
+        done = sorted({s[0] for s in steps if s[0] not in pre_reqs})[0]
+        result += done
+        steps = {step for step in steps if step[0] != done}
+        if len(steps) == 0:
+            return result + pre_reqs.pop()
 
-print(answer_1 + pre_reqs.pop())
+answer_1 = process(steps, 1, 0)
+print(answer_1)
+# answer_2 = process(steps, 2, 0)
 
 # Double sort?
