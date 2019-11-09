5ff0b6b39a1379e3c494675e84d2777f3cc9509b
Matt 88 <slainwarrior@hotmail.com>
2019-11-09 14:32:29 +0000

    Fix bug in 7a Python

--- 2018/7/main.py
+++ 2018/7/main.py
@@ -8,10 +8,21 @@ with open('test.txt') as f:
 
 def process(steps, workers, time):
     result = ""
+    workers = [("", 0) for x in range(workers)]
     while True:
         pre_reqs = {step[1] for step in steps}
-        done = sorted({s[0] for s in steps if s[0] not in pre_reqs})[0]
-        result += done
+        to_do = sorted({s[0] for s in steps if s[0] not in pre_reqs})
+
+        done = []
+        for i, (job, time) in enumerate(workers):
+            if time == 0:
+                done.append(job)
+                if to_do:
+                    do = to_do.pop(index=0)
+                    workers[i] = (do, 60)
+            else:
+                time -= 1
+
         steps = {step for step in steps if step[0] != done}
         if len(steps) == 0:
             return result + pre_reqs.pop()
