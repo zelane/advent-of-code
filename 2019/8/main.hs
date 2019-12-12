cd0666f8ad47bfbb49b2a37f2e3a0611ee30ccf4
Matt 88 <slainwarrior@hotmail.com>
2019-12-12 00:12:48 +0000

    Haskell day8

---
+++ 2019/8/main.hs
@@ -0,0 +1,26 @@
+import Data.List.Split
+import Data.List
+import Data.Function
+import Control.Parallel.Strategies
+
+
+
+
+img :: Char -> Char
+img c = if c == '1' then '#' else ' '
+ 
+squash :: String -> Char
+squash pixels = img $ head $ filter (/='2') pixels
+
+paint :: [String] -> String
+paint layers = unlines $ chunksOf 25 pixels
+    where
+        pixels  = map squash grouped
+        grouped = [map (!!x) layers | x <- [0..(25 * 6)-1]]
+
+main = do
+    pixels <- readFile "input.txt"
+    let layers = parMap rpar concat $ chunksOf 6 $ chunksOf 25 pixels
+    let answer1 = minimumBy (compare `on` (length.head)) $ map (group.sort) layers
+    print $ product.tail $ map length answer1
+    putStr $ paint layers
