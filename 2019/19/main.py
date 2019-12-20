2bb7840a89fbb81fe126d6b58bce2be8aa8d45d4
Matt 88 <slainwarrior@hotmail.com>
2019-12-20 00:19:52 +0000

    Python 19ab

--- 2019/19/main.py
+++ 2019/19/main.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
 from copy import copy
-from operator import itemgetter
-import time
 
 with open("input.txt") as f:
     prog = [int(x) for x in f.read().split(",")]
@@ -106,94 +104,29 @@ class Cpu:
         return outputs
 
 
-def render(pos, _map, route):
-    min_x = min(_map.keys(), key=itemgetter(0))[0]
-    max_x = max(_map.keys(), key=itemgetter(0))[0]
-    min_y = min(_map.keys(), key=itemgetter(1))[1]
-    max_y = max(_map.keys(), key=itemgetter(1))[1]
-
-    for y in reversed(range(min_y, max_y + 1)):
-        for x in reversed(range(min_x, max_x + 1)):
-            c = _map.get((x, y), "?")
-            if (x, y) == pos:
-                c = "*"
-            elif (x, y) in route:
-                c = "."
-            print(c, end="")
-        print("")
+def run(prog, x, y):
+    cpu = Cpu(copy(prog))
+    return cpu.run(inputs=[x, y])[0]
 
 
-trans = {1: (0, 1), 2: (0, -1), 3: (-1, 0), 4: (1, 0)}
-cells = {0: "#", 1: " ", 2: "O"}
+answer_1 = 0
+for y in range(0, 50):
+    for x in range(0, 50):
+        if run(prog, x, y) == 1:
+            answer_1 += 1
 
-_map = {}
-neigh = {(0, 0): {}}
-route = [(0, 0)]
-oxy_pos = None
+print(answer_1)
 
-pos = (0, 0)
-cpu = Cpu(copy(prog))
-
-while not cpu.halted:
-    backtrack = False
-    # Look at all possible transformations
-    for d, t in trans.items():
-        neigh_pos = (pos[0] + t[0], pos[1] + t[1])
-        # Never go back
-        if neigh_pos in route:
-            continue
-        # Try the first unexplored neighbour
-        if d not in neigh[pos]:
-            dire = d
-            break
-    # If we've explored everything we need to backtrack
-    else:
-        if pos == (0, 0):
-            break
-        backtrack = True
-        last = route[-2]
-        t = (-1 * (pos[0] - last[0]), -1 * (pos[1] - last[1]))
-        dire = [k for k, v in trans.items() if v == t][0]
-        route.pop()
-
-    output = cpu.run(inputs=[dire])[0]
-
-    cell = cells[output]
-    t = trans[dire]
-    cell_pos = (pos[0] + t[0], pos[1] + t[1])
-    _map[cell_pos] = cell
-    neigh[pos][dire] = cell
-
-    if backtrack:
-        pos = cell_pos
-
-    elif output in (1, 2):
-        pos = cell_pos
-        route.append(cell_pos)
-        if pos not in neigh:
-            neigh[pos] = {}
-
-        if output == 2:
-            oxy_pos = pos
-            print(len(route))
-
-render((0, 0), _map, [])
-
-minutes = 0
-while True:
-    updates = {}
-    for pos, c in _map.items():
-        if c == 'O':
-            for t in trans.values():
-                neigh_pos = (pos[0] + t[0], pos[1] + t[1])
-                if _map[neigh_pos] not in ("#", "O"):
-                    updates[neigh_pos] = "O"
-
-    if not updates:
-        print(minutes)
-        break
-
-    _map.update(updates)
-    minutes += 1
-    render((0, 0), _map, [])
-    time.sleep(0.1)
+# Narrowed down at least 2600
+for y in range(2060, 2100):
+    for x in range(0, 2100):
+        # Find first x val which is 1
+        if run(prog, x, y) == 1:
+            # Check if 100th coord to the right is also 1
+            if run(prog, x + 99, y) == 1:
+                # Check if 100th coord to the south is also 1
+                if run(prog, x, y + 99) == 1:
+                    print(x * 10000 + y)
+                    exit(1)
+            else:
+                break
