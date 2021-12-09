db7a9d4274251450cd1370bf64d34c24346facdc
Matt 88 <slainwarrior@hotmail.com>
2021-12-09 18:11:39 +0000

    \_ ..... o

--- 2021/9/main.hs
+++ 2021/9/main.hs
@@ -1,6 +1,6 @@
 import Data.Char (digitToInt)
-import Data.List (nub, sortOn)
-import Data.Map.Strict (Map, filterWithKey, fromList, keys, lookup, union, (!))
+import Data.List (sortOn, union)
+import Data.Map.Strict (Map, filterWithKey, fromList, keys, lookup, (!))
 import Data.Maybe (fromJust, isNothing, mapMaybe)
 import Data.Ord (Down (Down))
 import Prelude hiding (lookup)
@@ -9,33 +9,26 @@ type Point = (Int, Int)
 
 type Grid = Map Point Int
 
-neighbors :: Point -> [Point]
-neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-
 isLow :: Point -> Grid -> Bool
 isLow (x, y) grid = grid ! (x, y) < minimum neighs
   where
     neighs = mapMaybe (`lookup` grid) (neighbors (x, y))
+    neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
-traceLine :: (Int, Int) -> Point -> Int -> Grid -> [Point]
-traceLine (modX, modY) (x, y) v grid
+traceLine :: Point -> Grid -> (Int, Int) -> [Point]
+traceLine (x, y) grid (modX, modY)
   | isNothing nv = []
   | fromJust nv == 9 = []
-  | fromJust nv > v = (nx, ny) : traceLine (modX, modY) (nx, ny) (fromJust nv) grid
+  | fromJust nv > grid ! (x, y) = (nx, ny) : traceLine (nx, ny) grid (modX, modY)
   | otherwise = []
   where
     (nx, ny) = (x + modX, y + modY)
     nv = lookup (nx, ny) grid
 
 basin :: Grid -> Point -> [Point]
-basin grid (x, y) =
-  if null lines
-    then []
-    else lines ++ concatMap (basin grid) lines
+basin grid (x, y) = lines `union` concatMap (basin grid) lines
   where
-    points = mapMaybe (`lookup` grid) lines
-    v = grid ! (x, y)
-    lines = nub $ concatMap (\(mx, my) -> traceLine (mx, my) (x, y) v grid) trans
+    lines = concatMap (traceLine (x, y) grid) trans
     trans = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 main :: IO ()
@@ -47,5 +40,5 @@ main = do
   let lows = filterWithKey (\k v -> isLow k grid) grid
   print $ foldr (\a x -> a + x + 1) 0 lows
 
-  let basins = nub . basin grid <$> keys lows
+  let basins = basin grid <$> keys lows
   print $ product $ (+ 1) . length <$> take 3 (sortOn (Down . length) basins)
