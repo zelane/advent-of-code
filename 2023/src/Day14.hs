e77f23e5811bf11b126d455b399b510d15796716
Zelane <zelane@hollow.tech>
2023-12-15 19:42:45 +0000

    Tidy up day 14

--- 2023/src/Day14.hs
+++ 2023/src/Day14.hs
@@ -1,45 +1,37 @@
 module Day14 where
 
+import Data.Foldable (foldl')
 import Data.List (intercalate, iterate', sortBy, transpose)
 import Data.List.Split (splitOn)
+import Data.Text qualified as T
 
 tilt :: [String] -> [String]
 tilt s = transpose (sort' <$> transpose s)
 
 sort' :: String -> String
-sort' s = intercalate "#" $ sortBy sort'' <$> parts
+sort' s = intercalate "#" $ sortBy sort'' <$> splitOn "#" s
   where
-    parts = splitOn "#" s
+    sort'' '.' _ = GT
+    sort'' _ _ = EQ
 
-sort'' :: Char -> Char -> Ordering
-sort'' '#' _ = LT
-sort'' 'O' '#' = GT
-sort'' '.' _ = GT
-sort'' _ _ = EQ
-
-calc :: [(Int, String)] -> Int
-calc [] = 0
-calc ((i, x) : xs) = i * length (filter (== 'O') x) + calc xs
+calcLoad :: [String] -> Int
+calcLoad xs = foldl' (\a (i, x) -> a + calc i x) 0 $ zip [length xs, length xs - 1 ..] xs
+  where
+    calc i x = i * length (filter (== 'O') x)
 
 cycleLength :: (Eq a) => [a] -> (Int, Int)
-cycleLength [] = (0, 0)
-cycleLength (x : xs) =
-  let loop _ _ _ [] = (0, 0)
-      loop pow lam x (y : ys)
-        | x == y = (pow, lam)
-        | pow == lam = loop (2 * pow) 1 y ys
-        | otherwise = loop pow (1 + lam) x ys
-   in loop 1 1 x xs
+cycleLength (x : xs) = loop 1 1 x xs
+  where
+    loop pow lam x (y : ys)
+      | x == y = (pow, lam)
+      | pow == lam = loop (2 * pow) 1 y ys
+      | otherwise = loop pow (1 + lam) x ys
 
 solve :: IO String -> IO ()
 solve file = do
   input <- lines <$> file
-  let sorted = tilt input
-  print $ calc $ zip [length sorted, length sorted - 1 ..] sorted
-
-  let iter = iterate' (transpose . reverse . tilt) input
-  let loads = (\s -> calc $ zip [length s, length s - 1 ..] s) <$> iter
-  let (prefix, cycl) = cycleLength loads
-  let cyc = take cycl $ drop prefix loads
-  let idx = (4000000000 - prefix) `mod` cycl
-  print $ cyc !! idx
+  print $ calcLoad $ tilt input
+  let loads = calcLoad <$> iterate' (transpose . reverse . tilt) input
+  let (prefixL, cycleL) = cycleLength loads
+  let idx = prefixL + (4000000000 - prefixL) `mod` cycleL
+  print $ loads !! idx
