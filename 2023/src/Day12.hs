d5328daeb9b1352a44748cec81f08a6d1fedd2b4
Zelane <zelane@hollow.tech>
2023-12-16 02:05:59 +0000

    Reimplement day 12 with hashtable

--- 2023/src/Day12.hs
+++ 2023/src/Day12.hs
@@ -1,44 +1,53 @@
 module Day12 where
 
 import Data.Foldable (foldl')
-import Data.List (group, groupBy, intercalate)
+import Data.HashTable.IO qualified as H
+import Data.List (intercalate)
 import Data.List.Split (splitOn)
-import Data.Map qualified as M
-import Data.Text qualified as T
-import Debug.Trace (traceShow)
+import Data.Maybe (isJust)
 
-parse :: Char -> T.Text -> [Int] -> Int
-parse p s nums
-  | T.null s && not (null nums) = 0 -- too many nums
-  | null nums && T.elem '#' s = 0 -- need more nums
-  | null nums && T.all (== '.') s = 1 -- string ends in dots
+type Memo = H.BasicHashTable (Char, String, [Int]) Int
+
+parse :: Memo -> Char -> String -> [Int] -> IO Int
+parse memo p s nums
+  | null s && not (null nums) = return 0 -- too many nums
+  | null nums && elem '#' s = return 0 -- need more nums
+  | null nums && all (== '.') s = return 1 -- string ends in dots
   --
-  | a == '.' = parse '.' xs nums
-  | a == '?' && p == '#' = parse p (T.cons '.' xs) nums
-  | a == '?' = parse '.' (T.cons '#' xs) nums + parse p (T.cons '.' xs) nums
+  | a == '.' = parse' memo '.' xs nums
+  | a == '?' && p == '#' = parse' memo p ('.' : xs) nums
+  | a == '?' = (+) <$> parse' memo '.' ('#' : xs) nums <*> parse' memo p ('.' : xs) nums
   --
-  | a == '#' && p == '#' = 0
-  | a == '#' && n > T.length s = 0 -- num is longer than rest of input
-  | a == '#' && lenH > n = 0 -- hashes are longer than next num
-  | a == '#' && lenNextD < n = 0 -- length until next . too short
+  | a == '#' && p == '#' = return 0
+  | a == '#' && lenNextD < n = return 0 -- length until next . too short
   --
-  | a == '#' = parse '#' (T.drop (n - 1) xs) ns -- need to add .
-  --
+  | a == '#' = parse' memo '#' (drop (n - 1) xs) ns
   where
-    lenH = T.length $ T.takeWhile (== '#') s
-    lenNextD = T.length $ T.takeWhile (/= '.') s
-    a = T.head s
-    xs = T.tail s
+    (a : xs) = s
     (n : ns) = nums
+    lenNextD = length $ takeWhile (/= '.') s
+
+parse' :: Memo -> Char -> String -> [Int] -> IO Int
+parse' m c s xs = do
+  existing <- H.lookup m (c, s, xs)
+  maybe
+    ( do
+        next <- parse m c s xs
+        H.insert m (c, s, xs) next
+        return next
+    )
+    return
+    existing
 
 solve :: IO String -> IO ()
 solve file = do
   input <- lines <$> file
-  let springs = [(T.pack a, read <$> splitOn "," b :: [Int]) | line <- input, let [a, b] = words line]
-  let unfolded = [(T.intercalate "?" (replicate 5 s), concat $ replicate 5 c) | (s, c) <- springs]
+  let springs = [(a, read <$> splitOn "," b) | line <- input, let [a, b] = words line]
+  let unfolded = [(intercalate "?" (replicate 5 s), concat $ replicate 5 c) | (s, c) <- springs]
 
-  print $ sum $ uncurry (parse '.') <$> springs
-  print ""
-  -- print $ (\(a, b) -> parse '.' a b) $ unfolded !! 8
+  memo <- H.new
+  part1 <- mapM (uncurry (parse' memo '.')) springs
+  print $ sum part1
 
-  print $ foldl' (\acc (a, b) -> acc + parse '.' a b) 0 unfolded
+  part2 <- mapM (uncurry (parse' memo '.')) unfolded
+  print $ sum part2
